b4de3cb5dff4709e9a7cc13717fdb5a04b252ff6
Kai Nakamura <kaihnakamura@gmail.com>
2025-08-12 20:32:01 -0400

    Update

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -5,8 +5,11 @@
   home.homeDirectory = "/home/kai";
   home.stateVersion = "25.05";
 
+  nixpkgs.config.allowUnfree = true;
+
   imports = [
     (self + /modules/vim/vim.nix)
+    (self + /modules/vscode/vscode.nix)
   ];
 
   # Let Home Manager install and manage itself.
