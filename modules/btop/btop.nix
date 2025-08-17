5173b23baeb0d6088f865b8aaafce41122f85c98
Kai Nakamura <kaihnakamura@gmail.com>
2025-08-17 12:34:10 -0400

    btop

--- modules/btop/btop.nix
+++ modules/btop/btop.nix
@@ -6,42 +6,39 @@
       truecolor = true;
       theme_background = false;
       color_theme = "kai";
-      # update_ms = 1500;
-      # graph_symbol = "braille";
       show_battery = true;
     };
   };
 
-  # Custom btop theme leveraging global colorScheme
   home.file.".config/btop/themes/kai.theme".text = ''
-    theme[main_bg]="${config.colorScheme.palette.gray3}"
-    theme[main_fg]="${config.colorScheme.palette.white}"
-    theme[title]="${config.colorScheme.palette.white}"
-    theme[hi_fg]="${config.colorScheme.palette.red}"
-    theme[selected_bg]="${config.colorScheme.palette.gray2}"
-    theme[selected_fg]="${config.colorScheme.palette.white}"
-    theme[inactive_fg]="${config.colorScheme.palette.gray1}"
-    theme[graph_text]="${config.colorScheme.palette.white}"
-    theme[graph_line]="${config.colorScheme.palette.cyan}"
-    theme[cpu_box]="${config.colorScheme.palette.blue}"
-    theme[mem_box]="${config.colorScheme.palette.green}"
-    theme[net_box]="${config.colorScheme.palette.orange}"
-    theme[proc_box]="${config.colorScheme.palette.purple}"
-    theme[div_line]="${config.colorScheme.palette.gray2}"
-    theme[proc_misc]="${config.colorScheme.palette.yellow}"
-    theme[bg_proc]="${config.colorScheme.palette.gray2}"
+    theme[main_bg]="#${config.colorScheme.palette.gray3}"
+    theme[main_fg]="#${config.colorScheme.palette.white}"
+    theme[title]="#${config.colorScheme.palette.white}"
+    theme[hi_fg]="#${config.colorScheme.palette.red}"
+    theme[selected_bg]="#${config.colorScheme.palette.gray2}"
+    theme[selected_fg]="#${config.colorScheme.palette.white}"
+    theme[inactive_fg]="#${config.colorScheme.palette.gray1}"
+    theme[graph_text]="#${config.colorScheme.palette.white}"
+    theme[graph_line]="#${config.colorScheme.palette.cyan}"
+    theme[cpu_box]="#${config.colorScheme.palette.blue}"
+    theme[mem_box]="#${config.colorScheme.palette.green}"
+    theme[net_box]="#${config.colorScheme.palette.orange}"
+    theme[proc_box]="#${config.colorScheme.palette.purple}"
+    theme[div_line]="#${config.colorScheme.palette.gray2}"
+    theme[proc_misc]="#${config.colorScheme.palette.yellow}"
+    theme[bg_proc]="#${config.colorScheme.palette.gray2}"
 
-    theme[temp_start]="${config.colorScheme.palette.green}"
-    theme[temp_mid]="${config.colorScheme.palette.yellow}"
-    theme[temp_end]="${config.colorScheme.palette.red}"
-    theme[free_start]="${config.colorScheme.palette.green}"
-    theme[free_mid]="${config.colorScheme.palette.yellow}"
-    theme[free_end]="${config.colorScheme.palette.red}"
-    theme[cached_start]="${config.colorScheme.palette.cyan}"
-    theme[cached_mid]="${config.colorScheme.palette.blue}"
-    theme[cached_end]="${config.colorScheme.palette.purple}"
+    theme[temp_start]="#${config.colorScheme.palette.green}"
+    theme[temp_mid]="#${config.colorScheme.palette.yellow}"
+    theme[temp_end]="#${config.colorScheme.palette.red}"
+    theme[free_start]="#${config.colorScheme.palette.green}"
+    theme[free_mid]="#${config.colorScheme.palette.yellow}"
+    theme[free_end]="#${config.colorScheme.palette.red}"
+    theme[cached_start]="#${config.colorScheme.palette.cyan}"
+    theme[cached_mid]="#${config.colorScheme.palette.blue}"
+    theme[cached_end]="#${config.colorScheme.palette.purple}"
 
-    theme[warning]="${config.colorScheme.palette.orange}"
-    theme[critical]="${config.colorScheme.palette.red}"
+    theme[warning]="#${config.colorScheme.palette.orange}"
+    theme[critical]="#${config.colorScheme.palette.red}"
   '';
 }
