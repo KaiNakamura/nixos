ab9bd672ee0385e7fc320896d8fab32e357b469d
Kai Nakamura <kaihnakamura@gmail.com>
2025-08-16 21:17:31 -0400

    Revert vscode

--- modules/vscode/vscode.nix
+++ modules/vscode/vscode.nix
@@ -3,13 +3,5 @@
 {
   programs.vscode = {
     enable = true;
-    userSettings = {
-      # Use Noto Nerd Font for editor to see icons properly
-      "editor.fontFamily" = "'Noto Sans Mono Nerd Font', 'Droid Sans Mono', 'monospace', monospace";
-      
-      # Terminal font (also useful for integrated terminal)
-      "terminal.integrated.fontFamily" = "'Noto Sans Mono Nerd Font'";
-      "terminal.integrated.fontSize" = 14;
-    };
   };
 }
