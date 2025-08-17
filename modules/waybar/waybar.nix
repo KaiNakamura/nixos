0d5a127a9086d3d840e19c34e5e09416dd93062a
Kai Nakamura <kaihnakamura@gmail.com>
2025-08-17 18:50:15 -0400

    clock

--- modules/waybar/waybar.nix
+++ modules/waybar/waybar.nix
@@ -54,7 +54,7 @@
           tooltip-format = "CPU Usage: {usage}%";
         };
         clock = {
-          format = "{:%A, %B %e, %Y, %l:%M %p}";
+          format = "{:%A, %B %d, %Y • %I:%M %p}";
           tooltip = false;
         };
         battery = {
