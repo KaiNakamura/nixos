79f5f6f7213ee47a582cfd679572467a54117eb9
Kai Nakamura <kaihnakamura@gmail.com>
2025-08-16 21:55:19 -0400

    Add more nvim deps

--- modules/nvim/nvim.nix
+++ modules/nvim/nvim.nix
@@ -6,7 +6,7 @@ let
 in {
   programs.neovim = {
     enable = true;
-    extraPackages = with pkgs; [ fd ripgrep gcc ];
+    extraPackages = with pkgs; [ fd ripgrep gcc make unzip ];
   };
 
   # Clone/update the external editable repo before Home Manager links files
