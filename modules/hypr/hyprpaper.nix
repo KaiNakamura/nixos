a60d35baba4cec805cd9ed30d42cb686966325e2
Kai Nakamura <kaihnakamura@gmail.com>
2025-08-16 20:17:55 -0400

    wallpaper

--- modules/hypr/hyprpaper.nix
+++ modules/hypr/hyprpaper.nix
@@ -4,21 +4,14 @@
   services.hyprpaper = {
     enable = true;
     settings = {
-      # For now, we'll use a simple solid color
-      # You can add wallpapers later by modifying this configuration
       preload = [
-        # Add wallpaper paths here when you want to use them
-        # "/path/to/your/wallpaper.jpg"
+        "${./wallpapers/astronaut-jellyfish-monokai.jpg}"
       ];
       wallpaper = [
-        # For now, using default (solid color from Hyprland)
-        # ",/path/to/your/wallpaper.jpg"
+        ",${./wallpapers/astronaut-jellyfish-monokai.jpg}"
       ];
       splash = false;
       ipc = "on";
     };
   };
-  
-  # Create a wallpapers directory for future use
-  home.file."Pictures/Wallpapers/.keep".text = "";
 }
