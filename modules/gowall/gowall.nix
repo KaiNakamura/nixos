0567f23e6f4aca66bf3341d765ab7a9e0ab6f6e6
Kai Nakamura <kaihnakamura@gmail.com>
2025-08-16 20:10:35 -0400

    nix colors in gowall

--- modules/gowall/gowall.nix
+++ modules/gowall/gowall.nix
@@ -1,8 +1,31 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
-
   home.packages = [ pkgs.gowall ];
 
-  home.file.".config/gowall/config.yml".source = ./config.yml;
+  home.file.".config/gowall/config.yml".text = ''
+    themes:
+      - name: "kai"
+        colors:
+          - "#${config.colorScheme.palette.white}"
+          - "#${config.colorScheme.palette.gray0}"
+          - "#${config.colorScheme.palette.gray1}"
+          - "#${config.colorScheme.palette.gray2}"
+          - "#${config.colorScheme.palette.gray3}"
+          - "#${config.colorScheme.palette.black}"
+          - "#${config.colorScheme.palette.red}"
+          - "#${config.colorScheme.palette.dark_red}"
+          - "#${config.colorScheme.palette.orange}"
+          - "#${config.colorScheme.palette.dark_orange}"
+          - "#${config.colorScheme.palette.yellow}"
+          - "#${config.colorScheme.palette.dark_yellow}"
+          - "#${config.colorScheme.palette.green}"
+          - "#${config.colorScheme.palette.dark_green}"
+          - "#${config.colorScheme.palette.cyan}"
+          - "#${config.colorScheme.palette.dark_cyan}"
+          - "#${config.colorScheme.palette.blue}"
+          - "#${config.colorScheme.palette.dark_blue}"
+          - "#${config.colorScheme.palette.purple}"
+          - "#${config.colorScheme.palette.dark_purple}"
+  '';
 }
