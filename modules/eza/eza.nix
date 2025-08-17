d8b96e302dfab2fb82507f024c98cc227acc58ee
Kai Nakamura <kaihnakamura@gmail.com>
2025-08-17 14:01:58 -0400

    eza

--- modules/eza/eza.nix
+++ modules/eza/eza.nix
@@ -3,17 +3,11 @@
   programs.eza = {
     enable = true;
     enableZshIntegration = true;
-    extraOptions = [
-      "--group-directories-first"
-      "--icons"
-      "--header"
-      "--time-style=long-iso"
-    ];
   };
 
   programs.zsh.shellAliases = {
-    ls = "eza -a";
-    ll = "eza -al";
-    lt = "eza -a --tree --level=1";
+    ls = "eza -a --icons=always";
+    ll = "eza -al --icons=always";
+    lt = "eza -a --tree --level=1 --icons=always";
   };
 }
