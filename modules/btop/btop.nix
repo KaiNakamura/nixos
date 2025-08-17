fd65ba37cb926b24a6c917671e46565b6ad686b5
Kai Nakamura <kaihnakamura@gmail.com>
2025-08-17 12:26:06 -0400

    btop

--- modules/btop/btop.nix
+++ modules/btop/btop.nix
@@ -2,5 +2,10 @@
 {
   programs.btop = {
     enable = true;
+    settings = {
+      # Use truecolor and transparent background to blend with terminal theme
+      truecolor = true;
+      theme_background = false;
+    };
   };
 }
