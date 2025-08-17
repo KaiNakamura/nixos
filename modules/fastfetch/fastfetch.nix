7d15d7a98fabf42459cb24672496ca4639a0040a
Kai Nakamura <kaihnakamura@gmail.com>
2025-08-16 20:41:05 -0400

    remove ff settings

--- modules/fastfetch/fastfetch.nix
+++ modules/fastfetch/fastfetch.nix
@@ -3,49 +3,5 @@
 {
   programs.fastfetch = {
     enable = true;
-    settings = {
-      logo = {
-        source = "nixos";
-        padding = {
-          right = 1;
-        };
-      };
-      display = {
-        binaryPrefix = "si";
-        color = "blue";
-        separator = "  ";
-      };
-      modules = [
-        "title"
-        "separator"
-        "os"
-        "host"
-        "kernel"
-        "uptime"
-        "packages"
-        "shell"
-        "display"
-        "de"
-        "wm"
-        "wmtheme"
-        "theme"
-        "icons"
-        "font"
-        "cursor"
-        "terminal"
-        "terminalfont"
-        "cpu"
-        "gpu"
-        "memory"
-        "swap"
-        "disk"
-        "localip"
-        "battery"
-        "poweradapter"
-        "locale"
-        "break"
-        "colors"
-      ];
-    };
   };
 }
