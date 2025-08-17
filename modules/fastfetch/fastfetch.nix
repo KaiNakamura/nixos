31d02c6b603903265b9b1abeb55e467dc00b0667
Kai Nakamura <kaihnakamura@gmail.com>
2025-08-17 12:42:54 -0400

    ff

--- modules/fastfetch/fastfetch.nix
+++ modules/fastfetch/fastfetch.nix
@@ -4,4 +4,35 @@
   programs.fastfetch = {
     enable = true;
   };
+
+  home.file.".config/fastfetch/config.jsonc".text = ''
+  {
+    "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
+    "logo": {
+      "type": "small",
+      "padding": { "top": 1 }
+    },
+    "display": {
+      "separator": " "
+    },
+    "modules": [
+      { "key": "╭───────────╮", "type": "custom" },
+      { "key": "│  user    │", "type": "title", "format": "{user-name}" },
+      { "key": "│ 󰇅 hname   │", "type": "title", "format": "{host-name}" },
+      { "key": "│ 󰅐 uptime  │", "type": "uptime" },
+      { "key": "│ {icon} distro  │", "type": "os" },
+      { "key": "│  kernel  │", "type": "kernel" },
+      { "key": "│  wm      │", "type": "wm" },
+      { "key": "│ 󰇄 desktop │", "type": "de" },
+      { "key": "│  term    │", "type": "terminal" },
+      { "key": "│  shell   │", "type": "shell" },
+      { "key": "│ 󰍛 cpu     │", "type": "cpu" },
+      { "key": "│ 󰉉 disk    │", "type": "disk", "folders": "/" },
+      { "key": "│  memory  │", "type": "memory" },
+      { "key": "├───────────┤", "type": "custom" },
+      { "key": "│  colors  │", "type": "colors", "symbol": "circle" },
+      { "key": "╰───────────╯", "type": "custom" }
+    ]
+  }
+  '';
 }
