c8ca1dbd196e498fb291c9003e2c41bbd6d5cc02
Kai Nakamura <kaihnakamura@gmail.com>
2025-08-26 22:42:46 -0400

    Update homelab to use desktop for now

--- profiles/homelab/configuration.nix
+++ profiles/homelab/configuration.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ../../base/base.nix
+    ../../base/desktop.nix # TODO: Likely remove in the future
     ../../modules/secrets/secrets.nix
     ../../modules/tailscale/tailscale.nix
     ../../modules/k3s/k3s.nix
