876a7703a1d921f0408d0a98a5b881e9e91b7216
Kai Nakamura <kaihnakamura@gmail.com>
2025-08-26 22:48:55 -0400

    Fix homelab to desktop

--- profiles/homelab/home.nix
+++ profiles/homelab/home.nix
@@ -16,6 +16,22 @@
     ../../modules/fastfetch/fastfetch.nix
     ../../modules/lazydocker/lazydocker.nix
     ../../modules/k9s/k9s.nix
+
+    # TODO: Likely remove below
+    # Desktop
+    ../../modules/hypr/hypr.nix
+    ../../modules/waybar/waybar.nix
+    ../../modules/mako/mako.nix
+    ../../modules/wofi/wofi.nix
+    ../../modules/kitty/kitty.nix
+    ../../modules/power/power.nix
+
+    # Applications
+    ../../modules/obsidian/obsidian.nix
+    ../../modules/vscode/vscode.nix
+    ../../modules/mpv/mpv.nix
+    ../../modules/imv/imv.nix
+    ../../modules/gowall/gowall.nix
   ];
 
   home.username = "kai";
